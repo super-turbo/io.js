237c4176cd114d342e8d3f654e1475ddb162e32f
super-turbo <sugarhillmountain@gmail.com>
2015-04-07 06:02:46 -0400

    change file

--- deps/cares/src/ares_options.c
+++ deps/cares/src/ares_options.c
@@ -27,6 +27,9 @@
 #include "ares_inet_net_pton.h"
 #include "ares_private.h"
 
+/*
+*   new function
+*/
 
 int ares_get_servers(ares_channel channel,
                      struct ares_addr_node **servers)
